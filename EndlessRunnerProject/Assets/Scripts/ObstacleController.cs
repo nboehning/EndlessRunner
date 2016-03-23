d1c2f2c67bc7447836c2112a7abbb9f6f2e00110
svc-student <svc-student@student.directory.jccc>
2016-03-22 19:10:14 -0500

    Initial
    Initial Commit of project

---
+++ EndlessRunnerProject/Assets/Scripts/ObstacleController.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ObstacleController : MonoBehaviour
+{
+    GameObject obstaclePrefab;
+    float speed = -3.0f;
+    GameObject playerObj;
+
+	// Use this for initialization
+	void Start ()
+    {
+        Invoke("SpawnObstacle", 3f);
+	}
+	
+	void SpawnObstacle()
+    {
+        Vector3 spawnPos = new Vector3(10, Random.Range(-4, 0));
+        // Spawn an obstacle at x:10, y:-4 to 0
+        GameObject tempObstacle = Instantiate(obstaclePrefab, spawnPos, Quaternion.Euler(Vector3.zero)) as GameObject;
+        tempObstacle.name = "Obstacle(clone)";
+        tempObstacle.GetComponent<Rigidbody2D>().velocity = new Vector2(speed, 0);
+    }
+}
---
+++ EndlessRunnerProject/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour
+{
+    Animator anim;
+
+	// Use this for initialization
+	void Start ()
+    {
+        anim = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        MoveCharacter();
+	}
+
+    void MoveCharacter()
+    {
+        // Set parameter value for jump in animator based on W key.
+        if(Input.GetKeyDown(KeyCode.W))
+            anim.SetBool("jump", true);
+        else if(Input.GetKeyUp(KeyCode.W))
+            anim.SetBool("jump", false);
+        
+        // Set parameter value for slide in animator based on S key.
+        if(Input.GetKeyDown(KeyCode.S))
+            anim.SetBool("slide", true);
+        if(Input.GetKeyUp(KeyCode.S))
+            anim.SetBool("slide", false);
+
+
+    }
+}
