7f7524b25c3b9792894bb104ba49ea76e5e59e17
svc-student <svc-student@student.directory.jccc>
2016-03-24 19:16:15 -0500

    Finished in class
    Finished in class portion, found assets in OGA (links), brought in
    background assets

--- EndlessRunnerProject/Assets/Scripts/ObstacleController.cs
+++ EndlessRunnerProject/Assets/Scripts/ObstacleController.cs
@@ -3,21 +3,22 @@ using System.Collections;
 
 public class ObstacleController : MonoBehaviour
 {
-    GameObject obstaclePrefab;
+    public GameObject staticObstaclePrefab;
+    public GameObject movingObstaclePrefab;
     float speed = -3.0f;
     GameObject playerObj;
 
 	// Use this for initialization
 	void Start ()
     {
-        Invoke("SpawnObstacle", 3f);
+        InvokeRepeating("SpawnObstacle", 0f, 3f);
 	}
 	
 	void SpawnObstacle()
     {
-        Vector3 spawnPos = new Vector3(10, Random.Range(-4, 0));
+        Vector3 spawnPos = new Vector3(10, Random.Range(-3, 0));
         // Spawn an obstacle at x:10, y:-4 to 0
-        GameObject tempObstacle = Instantiate(obstaclePrefab, spawnPos, Quaternion.Euler(Vector3.zero)) as GameObject;
+        GameObject tempObstacle = Instantiate(staticObstaclePrefab, spawnPos, Quaternion.Euler(Vector3.zero)) as GameObject;
         tempObstacle.name = "Obstacle(clone)";
         tempObstacle.GetComponent<Rigidbody2D>().velocity = new Vector2(speed, 0);
     }
